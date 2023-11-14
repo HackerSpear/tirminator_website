b67aac33ba3b68d3a88e088d259e7b2c482ef65d
abdullah <codescept>
2023-11-14 13:04:54 +0500

    update new code in src

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,62 +1,92 @@
+// Language Toggle function
+    $(document).ready(function() {
+        // Check if language is stored in localStorage
+        if (localStorage.getItem('language') === 'cz') {
+        $('.language_toggle').val('cz');
+        $('[data-localize]').localize("language", { language: "cz" });
+        } else {
+        $('.language_toggle').val('en');
+        $('[data-localize]').localize("language", { language: "en" });
+        }
+     // Handle language toggle change
+        $('.language_toggle').on('change', function() {
+        let value = $(this).val();
+    
+        $('[data-localize]').localize("language", { language: value });
+        localStorage.setItem('language', value);
+        });
+    });
 
-$(document).ready(function () {
-  $('#nav-toggler').on('click', function () {
-    $(this).toggleClass('fa-times');
-    $('.main-nav .ext-stl').toggleClass('show');
+// select-2 select-box  
+    $(document).ready(function () {
+    $(".select2").select2();
+    });
+  // form validate
+  $(document).ready(function () {
+    $('#nav-toggler').on('click', function () {
+      $(this).toggleClass('fa-times');
+      $('.main-nav .ext-stl').toggleClass('show');
+    });
+  
+    $(window).on('scroll load', function () {
+  
+      if ($(window).scrollTop() > 0) {
+        $('.main-nav').addClass('bgchange');
+      } else {
+        $('.main-nav').removeClass('bgchange');
+      }
+  
+  
+    });
+  
   });
-
-  $(window).on('scroll load', function () {
-
-    if ($(window).scrollTop() > 0) {
-      $('.main-nav').addClass('bgchange');
-    } else {
-      $('.main-nav').removeClass('bgchange');
+  
+  function validateForm() {
+    // Get the input values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+  
+    // Clear previous error messages
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("subjectError").textContent = "";
+  
+    // Perform validation
+    var isValid = true;
+  
+    if (name.trim() === "") {
+      document.getElementById("nameError").textContent = "Please enter your name.";
+      isValid = false;
     }
-
-
-  });
-
-});
-
-function validateForm() {
-  // Get the input values
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var subject = document.getElementById("subject").value;
-
-  // Clear previous error messages
-  document.getElementById("nameError").textContent = "";
-  document.getElementById("emailError").textContent = "";
-  document.getElementById("subjectError").textContent = "";
-
-  // Perform validation
-  var isValid = true;
-
-  if (name.trim() === "") {
-    document.getElementById("nameError").textContent = "Please enter your name.";
-    isValid = false;
+  
+    if (email.trim() === "") {
+      document.getElementById("emailError").textContent = "Please enter your email.";
+      isValid = false;
+    }
+  
+    // Additional email validation using regular expression
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegex.test(email)) {
+      document.getElementById("emailError").textContent = "Please enter a valid email address.";
+      isValid = false;
+    }
+  
+    if (subject.trim() === "") {
+      document.getElementById("subjectError").textContent = "Please enter a subject.";
+      isValid = false;
+    }
+  
+    // Form is valid, allow submission
+    return isValid;
   }
+  
 
-  if (email.trim() === "") {
-    document.getElementById("emailError").textContent = "Please enter your email.";
-    isValid = false;
-  }
 
-  // Additional email validation using regular expression
-  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!emailRegex.test(email)) {
-    document.getElementById("emailError").textContent = "Please enter a valid email address.";
-    isValid = false;
-  }
 
-  if (subject.trim() === "") {
-    document.getElementById("subjectError").textContent = "Please enter a subject.";
-    isValid = false;
-  }
 
-  // Form is valid, allow submission
-  return isValid;
-}
+
+
 
 
 
